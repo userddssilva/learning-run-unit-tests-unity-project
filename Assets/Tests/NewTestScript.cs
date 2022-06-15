583f3df3ab53d1b77974b7170385cd9df2ffeb21
Dayvson Silva <userddssilva@gmail.com>
2022-06-15 00:24:38 -0400

    test: add unity test to the project

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator GameObject_WithRigidBody_WillBeAffectedByPhysics()
+    {
+        var go = new GameObject();
+        go.AddComponent<Rigidbody>();
+        var originalPosition = go.transform.position.y;
+
+        yield return new WaitForFixedUpdate();
+
+        Assert.AreNotEqual(originalPosition, go.transform.position.y);
+    }
+}
